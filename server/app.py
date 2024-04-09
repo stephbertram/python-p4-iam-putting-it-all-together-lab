c0e883a2bc1384347c626a352c00610173c7ed99
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-04-09 15:27:02 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,98 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+
+    def post(self):
+        try:
+            json = request.get_json()
+            user = User(
+                username = json.get('username'),
+                image_url = json.get('image_url'),
+                bio = json.get('bio')
+            )
+            # Use setter to set password
+            user.password_hash = json.get('password')
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return {'message': 'Invalid user'}, 422
+
+        session['user_id'] = user.id
+        return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {'message': '401: Not Authorized'}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+
+        password = request.get_json()['password']
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {"message": "401 Unauthorized"}, 401
 
 class Logout(Resource):
-    pass
-
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {'message': '204: No content'}, 204
+        
+        return {'message': '401: Unauthorized'}, 401
+    
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            all_recipes = [recipe.to_dict() for recipe in Recipe.query]
+            return all_recipes, 200
+        
+        return {'message': '401: Unauthorized'}, 401
+
+    def post(self):
+        if session.get('user_id'):
+            try: 
+                data = request.json
+                
+                new_recipe = Recipe(**data)
+                new_recipe.user_id = session['user_id']
+                db.session.add(new_recipe)
+                db.session.commit()
+                return new_recipe.to_dict(), 201
+            except Exception as e:
+                db.session.rollback()
+                return {"errors": str(e)}, 422 
+        
+        return {'message': '401: Unauthorized'}, 401
+    
+    # def post(self):
+    #     if session.get('user_id'):
+    #         try:
+    #             data = request.json
+    #             new_recipe = Recipe(
+    #                 title = data.get('title'),
+    #                 instructions = data.get('instructions'),
+    #                 minutes_to_complete = data.get('minutes_to_complete'),
+    #                 user_id = session['user_id']
+    #             )
+    #             db.session.add(new_recipe)
+    #             db.session.commit()
+    #             return new_recipe.to_dict(), 201
+    #         except Exception as e:
+    #             db.session.rollback()
+    #             return {"errors": str(e)}, 422 
+    
+    #     return {'message': '401: Unauthorized'}, 401
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
